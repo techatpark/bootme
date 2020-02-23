47a862048d25f0b5b6b45e3234dcfd54c6e0ce14
krishnaperumal <rkrishna16494@gmail.com>
2020-02-23 11:35:20 +0530

    bootstrap components added

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -3,24 +3,30 @@ import { action } from '@storybook/addon-actions';
 import { useEffect } from '@storybook/client-api';
 
 export default {
-  title: 'Demo',
+  title: 'button',
 };
 
-export const Heading = () => '<h1>Hello World</h1>';
-export const Headings = () =>
-  '<h1>Hello World</h1><h2>Hello World</h2><h3>Hello World</h3><h4>Hello World</h4>';
+// export const Heading = () => '<h1>Hello World</h1>';
+// export const Headings = () =>
+//   '<h1>Hello World</h1><h2>Hello World</h2><h3>Hello World</h3><h4>Hello World</h4>';
+
+export const Alert = () => '<div class="alert alert-primary" role="alert">A simple primary alert—check it out!</div>';
+
+export const Badge = () => '<button type="button" class="btn btn-primary">Notifications <span class="badge badge-light">4</span></button>';
+
+export const Breadcrumb = () => '<nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item active" aria-current="page">Home</li></ol>';
 
 export const Button = () => {
   const btn = document.createElement('button');
-  btn.innerHTML = 'Hello Button';
+  btn.setAttribute('class',"btn btn-primary");
+  btn.innerHTML = 'Primary';
   btn.addEventListener('click', action('Click'));
   return btn;
 };
 
-export const Effect = () => {
-  useEffect(() => {
-    document.getElementById('button').style.backgroundColor = 'yellow';
-  });
 
-  return '<button id="button">I should be yellow</button>';
-};
+export const Card = () => 
+'<div class="card" style="width: 18rem;"><img src="..." class="card-img-top" alt="..."> <div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>';
+
+
+
